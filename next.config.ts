a949dfddb737043b3116e55651b671f2181d84ba
TainakaR <131835658+TainakaR@users.noreply.github.com>
2026-04-14 00:47:41 +0900

    さらに

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: false,
+
+  // 【ここが重要】両方の設定を組み合わせて、jsdomを完全に外部扱いにする
+  serverExternalPackages: ["jsdom"],
   transpilePackages: ["jsdom"],
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.jp" },
-      {
-        protocol: "https",
-        hostname: "avataaars.io",
-      },
-      {
-        protocol: "https",
-        hostname: "w1980.blob.core.windows.net",
-      },
-      { protocol: "https", hostname: "placehold.jp" },
+      { protocol: "https", hostname: "avataaars.io" },
+      { protocol: "https", hostname: "w1980.blob.core.windows.net" },
       { protocol: "https", hostname: "images.microcms-assets.io" },
       {
         protocol: "https",
