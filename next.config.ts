4750881281b64f735967a4868b922c1f1190be3a
TainakaR <131835658+TainakaR@users.noreply.github.com>
2026-04-14 00:05:35 +0900

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  serverExternalPackages: ["jsdom"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.jp" },
